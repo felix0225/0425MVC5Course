ef7c23a2837e1bceeb243dfa3be4857632884a4e
felix chen <kib37704@outlook.com>
2015-04-25 14:07:26 +0800

    練習自訂一個 DataTypeAttribute 用以驗證特定數值欄位的數字必須為偶數

---
+++ MVC5Course/Models/MyValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class MyValidationAttribute : DataTypeAttribute
+    {
+        public MyValidationAttribute()
+            : base("MyValidation")
+        {
+            
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null) return false;
+            int num;
+            if (!int.TryParse(value.ToString(), out num)) return false;
+            return num%2 == 0;
+        }
+    }
+}
