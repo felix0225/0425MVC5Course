8d72d685b00aaf8b807331fe89438a3ee5a92967
felix chen <kib37704@outlook.com>
2015-04-25 12:09:16 +0800

    加入產生 Entity Framework 實體部分類別的 T4 程式碼產生器範本

--- MVC5Course/Models/ClientRepository.cs
+++ MVC5Course/Models/ClientRepository.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-	
+using System.Data.Entity.Core.Objects;
+
 namespace MVC5Course.Models
 {   
 	public  class ClientRepository : EFRepository<Client>, IClientRepository
@@ -14,6 +15,25 @@ namespace MVC5Course.Models
 	    public Client Find(int? id)
 	    {
 	        return this.All().FirstOrDefault(c => c.ClientId == id.Value);
+	    }
+
+        //stored procedure
+	    public ObjectResult<Product> QueryProducts()
+	    {
+	        return ((FabricsEntities) UnitOfWork.Context).QueryProduct();
+	    }
+
+	    public void Delete(Client client)
+	    {
+            //client.IsDelete = true;
+
+            var db = ((FabricsEntities) UnitOfWork.Context);
+	        foreach (var item in db.Order.ToList())
+	        {
+	            db.OrderLine.RemoveRange(item.OrderLine);
+	        }
+	        db.Order.RemoveRange(client.Order);
+
 	    }
 	}
 
