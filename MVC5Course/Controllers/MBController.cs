266eb6ca1bfba6227432d2dec10833307b360963
felix chen <kib37704@outlook.com>
2015-04-26 12:08:18 +0800

    練習 ViewData.Model , ViewData, ViewBag 與 TempData 的使用方式

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            ViewData["MyClient"] = db.Client.Find(100);
+
+            ViewData.Model = db.Product.Find(1);
+
+            return View();
+        }
+
+        public ActionResult TempData1()
+        {
+            ViewData["Message1"] = "Hello World 1";
+            TempData["Message2"] = "Hello World 2";
+            Session["Message3"] = "Hello World 3";
+
+            return RedirectToAction("TempData2");
+        }
+        public ActionResult TempData2()
+        {
+            ViewBag.Message1 = ViewData["Message1"];
+            ViewBag.Message2 = TempData["Message2"];
+            ViewBag.Message3 = Session["Message3"];
+
+            return View();
+        }
+    }
+}
