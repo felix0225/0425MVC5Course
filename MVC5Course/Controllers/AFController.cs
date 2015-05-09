4d8310933804ae7a2ca94828579965e16219b38d
felix chen <kib37704@outlook.com>
2015-05-09 09:04:44 +0800

    練習 [HandleError] 自訂錯誤頁面的做法

---
+++ MVC5Course/Controllers/AFController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AFController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HandleError]
+        //public ActionResult ShowError()
+        //{
+        //    throw new Exception("OK");
+        //    return View();
+        //}
+
+        [HandleError(ExceptionType = typeof(ArgumentException), View = "Error.Argument")]
+        public ActionResult ShowError(string type)
+        {
+            if (type == "1")
+            {
+                throw new ArgumentException("MyBad");
+            }
+            else
+            {
+                throw new Exception("OK");
+            }
+
+            return View();
+        }
+    }
+}
