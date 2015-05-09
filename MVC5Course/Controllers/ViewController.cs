63a07c0dec61cfe827a1d23507bcd33cf34139df
felix chen <kib37704@outlook.com>
2015-05-09 10:56:12 +0800

    用 Razor 輸出 ViewBag.num = new int[] { 1,2,3,4,5 } 陣列的內容

---
+++ MVC5Course/Controllers/ViewController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public ActionResult Index()
+        {
+            ViewBag.num = new int[] {1, 2, 3, 4, 5};
+
+            return PartialView();
+        }
+    }
+}
