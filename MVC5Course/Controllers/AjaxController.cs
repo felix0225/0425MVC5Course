93c4862a970c374a333cf6e6d7bfdff8919d6159
felix chen <kib37704@outlook.com>
2015-05-10 10:46:27 +0800

    練習其他各種不同的 AjaxOptions 參數使用

--- MVC5Course/Controllers/AjaxController.cs
+++ MVC5Course/Controllers/AjaxController.cs
@@ -18,5 +18,10 @@ namespace MVC5Course.Controllers
         {
             return DateTime.Now.ToString();
         }
+
+        public ActionResult GetJson()
+        {
+            return Json(new {result="OK"});
+        }
     }
 }
